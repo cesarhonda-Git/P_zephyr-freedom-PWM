3e610adc6e723970c526e42f74714d6e2f271d3a
cesarhonda <cesarhonda@usp.br>
2026-04-14 09:10:56 -0300

    commit inicial: motor pwm

--- src/main.c
+++ src/main.c
@@ -5,20 +5,24 @@
 
 #define SLEEP_TIME_MS 1000 //intervalo de 1 segundo
 
-// Define o LED usando Device Tree
-#define LED0_NODE DT_ALIAS(led0)
+const struct device *gpioa_dev = DEVICE_DT_GET(DT_NODELABEL(gpioa)); //devise tree
+const struct device *gpiob_dev = DEVICE_DT_GET(DT_NODELABEL(gpiob));
+
+
 
 // Define o valor do registrador MOD do TPM para configurar o período do PWM
 #define TPM_MODULE 1000         // Define a frequência do PWM fpwm = (TPM_CLK / (TPM_MODULE * PS))
 // Valores de duty cycle correspondentes a diferentes larguras de pulso
+uint16_t duty_100 = 0;
+uint16_t duty_75 = TPM_MODULE/4; 
 uint16_t duty_50  = TPM_MODULE/2;       // 50% de duty cycle (meio brilho)
-uint16_t duty_75 = TPM_MODULE/4 *3;
-uint16_t duty_100 = TPM_MODULE;
+uint16_t duty_25 = TPM_MODULE/4 *3;
+uint16_t duty_0 = TPM_MODULE;
 
 
 int main(void)
 {
-    int red = 0, green = 1;
+    int ondi = 1, ones = 2;
     // Inicializa o módulo TPM2 com:
     // - base do TPMx
     // - fonte de clock PLL/FLL (TPM_CLK)
@@ -26,27 +30,41 @@ int main(void)
     // - tipo de clock (TPM_CLK)
     // - prescaler de 1 a 128 (PS)
     // - modo de operação EDGE_PWM
-    pwm_tpm_Init(TPM2, TPM_PLLFLL, TPM_MODULE, TPM_CLK, PS_128, EDGE_PWM);
+    pwm_tpm_Init(TPM0, TPM_PLLFLL, TPM_MODULE, TPM_CLK, PS_128, EDGE_PWM);
 
-    // Inicializa o canal 0 do TPM2 para gerar sinal PWM na porta GPIOB_18
+    // Inicializa o canal 0 do TPM0 para gerar sinal PWM na porta GPIOB_18
     // - modo TPM_PWM_H (nível alto durante o pulso)
-    pwm_tpm_Ch_Init(TPM2, red, TPM_PWM_H, GPIOB, 18);
+    pwm_tpm_Ch_Init(TPM0, ondi, TPM_PWM_H, GPIOA, 4); //enab direita
+    pwm_tpm_Ch_Init(TPM0, ones, TPM_PWM_H, GPIOA, 5); //enab esquerda
 
-    pwm_tpm_Ch_Init(TPM2, green, TPM_PWM_H, GPIOB, 19);
+    gpio_pin_configure(gpioa_dev, 12, GPIO_OUTPUT_ACTIVE);
+    gpio_pin_configure(gpioa_dev, 13, GPIO_OUTPUT_INACTIVE);
+    gpio_pin_configure(gpiob_dev, 0, GPIO_OUTPUT_ACTIVE);
+    gpio_pin_configure(gpiob_dev, 1, GPIO_OUTPUT_INACTIVE);
 
-   
-    
     // Loop infinito
     for (;;)
     {
         // O programa poderia alterar o duty cycle dinamicamente aqui se desejado
-        pwm_tpm_CnV(TPM2, green, duty_75);
-        pwm_tpm_CnV(TPM2, red, duty_50);
-        k_msleep(SLEEP_TIME_MS);
-         // Define o valor do duty cycle: nesse caso, duty_100 (LED quase desligado)
-        pwm_tpm_CnV(TPM2, green, duty_100);
-        pwm_tpm_CnV(TPM2, red, duty_100);
-        k_msleep(SLEEP_TIME_MS);
+        pwm_tpm_CnV(TPM0, ondi, duty_0);
+        pwm_tpm_CnV(TPM0, ones, duty_0);
+        k_msleep(SLEEP_TIME_MS*3);
+        
+        pwm_tpm_CnV(TPM0, ondi, duty_25);
+        pwm_tpm_CnV(TPM0, ones, duty_25);
+        k_msleep(SLEEP_TIME_MS*3);
+
+        pwm_tpm_CnV(TPM0, ondi, duty_50);
+        pwm_tpm_CnV(TPM0, ones, duty_50);
+        k_msleep(SLEEP_TIME_MS*3);
+
+        pwm_tpm_CnV(TPM0, ondi, duty_75);
+        pwm_tpm_CnV(TPM0, ones, duty_75);
+        k_msleep(SLEEP_TIME_MS*3);
+
+        pwm_tpm_CnV(TPM0, ondi, duty_100);
+        pwm_tpm_CnV(TPM0, ones, duty_100);
+        k_msleep(SLEEP_TIME_MS*3);
     }
 
     return 0;
