8af3672c85de5220f991e3bd8aa02c20b3096110
cesarhonda <cesarhonda@usp.br>
2026-04-15 10:07:47 -0300

    commit base

--- src/main.c
+++ src/main.c
@@ -2,29 +2,3 @@
 #include <device.h>
 #include <drivers/gpio.h>
 
-#define INPUT_PORT  "GPIO_4"   // Porta E = GPIO_4 no seu .dts
-#define INPUT_PIN   20         // PTE20
-
-void main(void)
-{
-    const struct device *input_dev;
-    int ret, val;
-
-    input_dev = device_get_binding(INPUT_PORT);
-    if (!input_dev) {
-        printk("Erro ao acessar porta %s\n", INPUT_PORT);
-        return;
-    }
-
-    ret = gpio_pin_configure(input_dev, INPUT_PIN, GPIO_INPUT);
-    if (ret != 0) {
-        printk("Erro ao configurar pino %d\n", INPUT_PIN);
-        return;
-    }
-
-    while (1) {
-        val = gpio_pin_get(input_dev, INPUT_PIN);
-        printk("Valor do PTE20: %d\n", val);
-        k_msleep(500);
-    }
-}
